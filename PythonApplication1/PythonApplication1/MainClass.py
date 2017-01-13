7f8c11bab26a311734ad2232e9cfc3a78146e2d1
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-01-13 12:21:31 +0900

    ファイルの作成
    メインクラスのテストで使用するテキストファイルを作成しました。
    また、そのファイルをメインクラスで読み込むように変更しました。

--- PythonApplication1/PythonApplication1/MainClass.py
+++ PythonApplication1/PythonApplication1/MainClass.py
@@ -1,6 +1,18 @@
-class MainClass(object):
-    """description of class"""
+# -*- encode:utf-8 -*-
+
+import sys
+
+class MainClass:
+	"""message"""
+
+	def __init__(self,filename):
+		self.textList = []
+		with open(filename,"rt") as f:
+			self.textList = f.readlines()
+		return
 
 if __name__ == "__main__":
 	print "start main class"
+	filename = "input_main.txt"
+	main = MainClass(filename)
 
