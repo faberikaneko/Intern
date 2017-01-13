2c956e887dc1ebdc1ac5ec5a5e98a1ad41e9666b
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-01-13 12:42:43 +0900

    テキスト分割
    テキストを句点で分割したい。

--- PythonApplication1/PythonApplication1/MainClass.py
+++ PythonApplication1/PythonApplication1/MainClass.py
@@ -1,18 +1,30 @@
 # -*- encode:utf-8 -*-
 
 import sys
+import re
 
 class MainClass:
 	"""message"""
 
-	def __init__(self,filename):
+	def __init__(self,filename,outputname):
 		self.textList = []
 		with open(filename,"rt") as f:
 			self.textList = f.readlines()
+		self.sectionList = []
+#		pre = re.compile(r'[.。]')
+		for text in self.textList:
+#			self.sectionList.append(pre.split(text))
+			self.sectionList.append(text.split("。"))
+		with open(outputname,"wt") as f:
+			for line in self.sectionList:
+				for text in line:
+					f.write(text)
+		self.sectionList
 		return
 
 if __name__ == "__main__":
 	print "start main class"
 	filename = "input_main.txt"
-	main = MainClass(filename)
+	outputname = "output_main.txt"
+	main = MainClass(filename,outputname)
 
