1cd3ed085aefd00d4422583c97994a8a9dc2e03e
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-01-16 11:56:42 +0900

    codec 処理
    辞書作成時のcodec問題を解消

--- ImageSuggestion/ImageSuggestion/DifficultyEstimationClass.py
+++ ImageSuggestion/ImageSuggestion/DifficultyEstimationClass.py
@@ -7,122 +7,124 @@ import csv
 
 class DifficultyEstimationClass:
 
-    fcorpus = []
-    keyword = {}
-    
-    hitcount = 0
-    difficulty = []
+	fcorpus = []
+	keyword = {}
+	
+	hitcount = 0
+	difficulty = []
 
-    word = [ ]
+	word = [ ]
 
-    def openInputFile(self, filename='./input.txt'):
-        # Open input file
-        self.fin = open(filename,'r')
+	def openInputFile(self, filename='./input.txt'):
+		# Open input file
+		self.fin = open(filename,'r')
 
-    def closeInputFile(self):
-         #Close input file
-        self.fin.close()
+	def closeInputFile(self):
+		 #Close input file
+		self.fin.close()
 
-    def openCorpusFile(self):
-        # Open corpus file
-        for i in range(1,13):
-            self.fcorpus.append(open('./corpus/kanji_level'+str(i)+'.csv',"r"))
+	def openCorpusFile(self):
+		# Open corpus file
+		for i in range(1,13):
+			self.fcorpus.append(open('./corpus/kanji_level'+str(i)+'.csv',"r"))
 
-    def closeCorpusFile(self):
-        # Close corpus files
-        for fi in self.fcorpus:
-            fi.close()
+	def closeCorpusFile(self):
+		# Close corpus files
+		for fi in self.fcorpus:
+			fi.close()
 
-    def openOutputFile(self, filename='./output.txt'):
-        # Open output file
-        self.fout = open('./result.txt','w')
+	def openOutputFile(self, filename='./output.txt'):
+		# Open output file
+		self.fout = open('./result.txt','w')
 
-    def closeOutputFile(self):
-        # Close output file
-        self.fout.close()
+	def closeOutputFile(self):
+		# Close output file
+		self.fout.close()
 
-    def makeDictionary(self):
+	def makeDictionary(self):
 
-        # sysモジュールをリロードする
-        reload(sys)
-        # デフォルトの文字コードを変更する．
-        sys.setdefaultencoding('utf-8')
-        # デフォルトの文字コードを出力する．
-        print 'defaultencoding:', sys.getdefaultencoding()
+		# sysモジュールをリロードする
+		reload(sys)
+		# デフォルトの文字コードを変更する．
+		sys.setdefaultencoding('utf-8')
+		# デフォルトの文字コードを出力する．
+		print 'defaultencoding:', sys.getdefaultencoding()
 
-        # Set corpus character and difficulty to keyword
-        count = 0
-        for fc in self.fcorpus:
-            reader = csv.reader(fc)
-            for row in reader:
-               DifficultyEstimationClass.keyword[row[0]] = row[1]
-#               print row[0] + " : " + self.keyword[count][row[0]]
-            count += 1
-        print "END MAKING DICTIONARY"
-        
-#        # Test
-#        samplechar = "一"
-#        sam = samplechar.decode('utf-8')
-#        count = 0
-#        for i in self.keyword:
-##            print self.keyword[count].keys()
-#            for s in self.keyword[count].keys():
-#                print isinstance(samplechar,str)
-#                print s + "  " + samplechar
-#                if s==samplechar:
-#                    print "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-##                print "HIT"
-#            count += 1
+		# Set corpus character and difficulty to keyword
+		for fc in self.fcorpus:
+			reader = csv.reader(fc)
+			codec = "utf-8"
+			for row in reader:
+				try:
+					DifficultyEstimationClass.keyword[row[0].decode(codec)] = row[1].decode(codec)
+				except UnicodeDecodeError:
+					codec = "shift-jis"
+					DifficultyEstimationClass.keyword[row[0].decode(codec)] = row[1].decode(codec)
+#			print row[0] + " : " + self.keyword[count][row[0]]
+		print "END MAKING DICTIONARY"
+		
+#		# Test
+#		samplechar = "一"
+#		sam = samplechar.decode('utf-8')
+#		count = 0
+#		for i in self.keyword:
+##			print self.keyword[count].keys()
+#			for s in self.keyword[count].keys():
+#				print isinstance(samplechar,str)
+#				print s + "  " + samplechar
+#				if s==samplechar:
+#					print "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+##				print "HIT"
+#			count += 1
 
-    def splitSentences(self):
-        # Split sentences to character
-        word = [ ]
-        for line in self.fin.read().decode('utf-8'):
-            for i in line:
-                word.append(i)#.encode('utf-8'))
-                self.fout.write(i.encode('utf-8')+" ")
-    
-    def estimateDifficulty(self):
-        # Start Checking
-        print "CHECK"
+	def splitSentences(self,sentence):
+		# Split sentences to character
+		self.word = [ ]
+		for char in sentence:
+			word.append(char)
+			word
+	
+	def estimateDifficulty(self):
+		# Start Checking
+		print "CHECK"
 
-        for w in word:
-            for fc in fcorpus:
-        #        print i
-                reader = csv.reader(fc)
-        #        header = next(reader)
-                for row in reader:
-        #            print row[0]
-        #            print w
-                    if w==row[0]:
-                        hitcount+=1
-                        difficulty.append(row[2])
-                        print "HIT"
-            print "END CORPUS"
-        print "END SENTENCE"
-    
+		for w in word:
+			for fc in fcorpus:
+		#		print i
+				reader = csv.reader(fc)
+		#		header = next(reader)
+				for row in reader:
+		#			print row[0]
+		#			print w
+					if w==row[0]:
+						hitcount+=1
+						difficulty.append(row[2])
+						print "HIT"
+			print "END CORPUS"
+		print "END SENTENCE"
+	
 
-    def open(self):
-        self.openInputFile()
-        self.openCorpusFile()
-        self.openOutputFile()
+	def open(self):
+		self.openInputFile()
+		self.openCorpusFile()
+		self.openOutputFile()
 
-    def close(self):
-        self.closeInputFile()
-        self.closeCorpusFile()
-        self.closeOutputFile()
+	def close(self):
+		self.closeInputFile()
+		self.closeCorpusFile()
+		self.closeOutputFile()
 
 
 
 
 """
 if __name__ == "__main__":
-    print "start DifficultyEstimation"
-    this = DifficultyEstimation()
-    this.open()
-    this.makeDictionary()
-    this.splitSentences()
-    this.close()
-    print "THE END"
-    """
-    
+	print "start DifficultyEstimation"
+	this = DifficultyEstimation()
+	this.open()
+	this.makeDictionary()
+	this.splitSentences()
+	this.close()
+	print "THE END"
+	"""
+	
