2cdc6590731f318853b0d89a58689f531ba6e3c3
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-01-16 11:41:05 +0900

    メイン関数の整理
    ファイルの入力と出力を調節（関数にまとめた）

--- ImageSuggestion/ImageSuggestion/MainClass.py
+++ ImageSuggestion/ImageSuggestion/MainClass.py
@@ -5,53 +5,67 @@ import re
 
 import ScoringClass
 import DifficultyEstimationClass
+from DifficultyEstimationClass import DifficultyEstimationClass
 
 class MainClass:
 	"""message"""
 
-	def __init__(self,imputfile,outputfile):
+	minLineNumber = 1
+
+	def __init__(self):
 		# Change default character encoding
 		reload(sys)                                        #Reload module
 		sys.setdefaultencoding('utf-8')     #Set character encoding to 'utf-8'
 
-        # Set sentences to textList from inputfile
-		self.textList = []
-		with open(imputfile,"rt") as file:
-            # Set sentences encoded 'utf-8' from input file to textList
-			self.textList = map(lambda t:t.decode("utf-8"),file.readlines())
-        
-        # Split by '。', '\.', '\n' sentences and set to sectionList from textList
-		self.sectionList = []
-		pre = re.compile(ur'[。\.\n]')
-		for text in self.textList:
-			self.sectionList.append(filter(lambda t:len(t) > 0,pre.split(text)))
 		return
 
+	def readfile(self,imputfilename):
+		"""read imput file from imputfilename(file) to rawFile and sectionList and sentenceList"""
+		with open(imputfilename,"rt") as file:
+			# Set sentences encoded 'utf-8' from input file to textList
+			self.rawFile = file.read().decode("utf-8")
+			# split rawFile to sectionList[] by "\n" 
+			self.sectionList = filter(lambda t:len(t) > 0,map(lambda t:t.replace("\n",""),self.rawFile.split(u"\n")))
+			# split foreach sectionList[] to sentenceList by "。\.．"
+			self.sentenceList = []
+			pre = re.compile(ur'[。\.．]')
+			for sentence in self.sectionList:
+				self.sentenceList.append(filter(lambda t:len(t) > 0,pre.split(sentence)))
+		return
+
+	def writefile(self,outputfilename):
+		return
 
 	def do(self):
 		sc = ScoringClass
 		scores = []
-		for section in filter(lambda s : len(s) > 3,self.sectionList):
+		for section in filter(lambda s : len(s) > MainClass.minLineNumber,self.sectionList):
 			print "yes"
-			scores.append(this.scoreSentenceList(section))
+			scores.append(sc.scoreSentenceList(section))
 		return scores
 
 if __name__ == "__main__":
 	print "start main class"
-	imputfile = "input_main.txt"
-	outputfile = "output_main.txt"
+	imputfilename = "input_main.txt"
+	outputfilename = "output_main.txt"
 
     # Initialize MainClass and make sectionList
-	main = MainClass(imputfile,outputfile)
+	main = MainClass()
+	main.readfile(imputfilename)
 
     # TODO: Difficulty Estimation
+
+	dec = DifficultyEstimationClass()
+	dec.openCorpusFile()
+	dec.makeDictionary()
+	sentenceDifficultyDict = dec
 
     # Scoring to section
 	scores = main.do()
 
     # 
-	with open(outputfile,"wt") as f:
+	with open(outputfile,"wt") as file:
 		for line in main.sectionList:
 			for text in line:
-				f.write(text.encode("utf-8")+"\n")
-			f.write("yes\n" if len(line)>2 else "no\n")
+				file.write(text.encode("utf-8")+"\n")
+			file.write("yes\n" if len(line)>2 else "no\n")
