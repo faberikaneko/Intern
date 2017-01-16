96ffe2918f0c1c8969b0e84a6f07bffed821cf45
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-01-16 16:36:06 +0900

    やっとできました
    文章内のキーワードと表現を検索して個数と重みを計算した。

--- PythonApplication1/PythonApplication1/ScoringClass.py
+++ PythonApplication1/PythonApplication1/ScoringClass.py
@@ -4,6 +4,7 @@ import MeCab
 import sys
 import codecs
 import csv
+import re
 
 class ScoringClass:
 	"""scoring sentense"""
@@ -32,31 +33,46 @@ class ScoringClass:
 				next(reader)
 				#dataCの表現部分：重み辞書を作成
 				for row in reader:
-					ScoringClass.keysentence[row[0].replace("～","*").decode("utf-8")] = int(row[2].decode("utf-8"))
+					sentence = row[0].replace("～",".*").decode("utf-8")
+					sentence = sentence if sentence.startswith(r".*") else sentence
+					ScoringClass.keysentence[sentence] = int(row[2].decode("utf-8"))
+			for key in ScoringClass.keysentence.keys():
+				print "%s,%d"%(key,ScoringClass.keysentence[key])
 		return
 
 	#分割する文章を読み込む
-	def scoreSentence(self,text):
-		point = 0
+	def scoreSentenceByWord(self,text):
+		point = []
 		m = MeCab.Tagger("-Owakati")
 		node = m.parseToNode(text.encode("utf-8"))
 		ans = ""
 		node = node.next
+		nodeList = []
 		while node.next:
-			word = (node.surface.decode("utf-8"),node.feature.decode("utf-8"))
-			ans += "%s %s\n"%word
-			if word[0] in ScoringClass.clueword.keys():
-				point += ScoringClass.clueword[word[0]]
-#				print"%s %d"%(node.surface.decode("utf-8"),ScoringClass.clueword[word[0]])
+			surface = node.surface.decode("utf-8")
+			nodeList.append(surface)
+#			nodeList.append(nono.feature.decode("utf-8"))
+#			word = (node.surface.decode("utf-8"),node.feature.decode("utf-8"))
+#			ans += "%s %s\n"%word
+			if surface in ScoringClass.clueword.keys():
+				point.append(ScoringClass.clueword[surface])
 			node = node.next
-		print point
+		return point
+
+	def scoreSentenceByExp(self,text):
+		point = []
+		for sentence in ScoringClass.keysentence.keys():
+			if re.match(sentence,text):
+				print "match!"
+				point.append(ScoringClass.keysentence[sentence])
 		return point
 
 	def scoreSentenceList(self,textList):
 		scoreList = []
 		for text in textList:
-			score = self.scoreSentence(text)
-			scoreList.append((text,score))
+			scoreWord = self.scoreSentenceByWord(text)
+			scoreExp = self.scoreSentenceByExp(text)
+			scoreList.append((text,scoreWord,scoreExp))
 		return scoreList
 
 	def __init__(self):
@@ -71,13 +87,19 @@ if __name__ == "__main__":
 	this = ScoringClass()
 	textList = []
 	filename = "imput_scoring.txt"
-	with open(filename,"rt") as file:
-		textList = file.readlines()
-	scores = this.scoreSentenceList(map(lambda t : t.replace("\n","").decode("utf-8") , textList))
+	try:
+		with codecs.open(filename,"r",encoding="utf-8-sig") as file:
+			textList = file.readlines()
+	except UnicodeDecodeError:
+		with codecs.open(filename,"r",encoding="shift-jis") as file:
+			textList = file.readlines()
+	text = textList[0]
+	textList = map(lambda t:t.replace(u"\r\n" or u"\r" or u"\n",""),textList)
+	scores = this.scoreSentenceList(textList)
 	filename = "output_scorig.txt"
-	with open(filename,"wt") as f:
+	with open(filename,"wt") as file:
 		for score in scores:
-			f.write(score[0] + ":" + str(score[1]) + "\n")
+			file.write("%s:%d/%d,%d/%d\n"%(score[0],sum(score[1]),len(score[1]),sum(score[2]),len(score[2])))
 	
 
 	#print(key)
