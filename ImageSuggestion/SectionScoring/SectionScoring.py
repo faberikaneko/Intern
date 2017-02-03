fc65114c8c347463287a1f77eed3b827fca58f80
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-02-03 10:40:10 +0900

    comment message
    コメントプログラミング（初）

--- ImageSuggestion/SectionScoring/SectionScoring.py
+++ ImageSuggestion/SectionScoring/SectionScoring.py
@@ -1,7 +1,30 @@
 # -*- encoding:utf-8 -*-
 
-"""Scoring to sections about "Image Power".
+"""Scoring to sections about 'Image Power'.
     score object:
     *Paralell Sentence/Word
     *Clueword
     *Sentence expression"""
+
+#HTMLFile -> HTMLText:unicode
+
+#HTMLText:unicode -> RawText:unicode (and more?)
+
+#RawText:unicode -> Sections:list(sptext?unicode)
+
+#for Section in Sections:
+    #Section(sptext?unicode) -> Sentences:list(sptext?unicode)
+    #for Sentence in Sentences:
+        #*has Paralell?
+            #is about numerical? or sentential?
+        #*has ClueWord?
+        #*has keyExpression?
+        #*is Description about paralell word?
+    #sum score and set in Section
+
+#sort Sections by score
+
+#show answer and...
+
+
+# F I N I S H ! ( O S H I M A I ! )
