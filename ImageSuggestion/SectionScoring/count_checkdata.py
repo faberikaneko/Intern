0f168c1c73e4740a5e7b8bb55f43af1bd6458adc
faberiyokouchi <faberi.yokouchi@gmail.com>
2017-02-20 17:21:36 +0900

    new3_tf_0.0

--- ImageSuggestion/SectionScoring/count_checkdata.py
+++ ImageSuggestion/SectionScoring/count_checkdata.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     point = 0
     alls = 0
     argv = sys.argv
-    outdirname = u"new3_tf_only_81to100"
+    outdirname = u"new3_tf"
     if not os.path.exists(outdirname):
         os.mkdir(outdirname)
     for dirname in dirnames:
